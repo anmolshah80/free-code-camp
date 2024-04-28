256b4c8e28f41142e205482b0a16bf1f7090ff07
Anmol Shah <anmolsha80@gmail.com>
2024-04-28 22:16:00 +0545

    features/project-8-theme-switch - Implement the feature to toggle between light and dark modes using a button - Create a custom hook called useLocalStorage to set and update the value of current theme in localStorage and change the UI based on that value

--- theme-switch/src/components/index.jsx
+++ theme-switch/src/components/index.jsx
@@ -1,9 +1,23 @@
-import './styles.css';
+import useLocalStorage from './useLocalStorage';
+
+import './theme.css';
 
 const ThemeSwitch = () => {
+  const [theme, setTheme] = useLocalStorage('theme', 'dark');
+
+  const handleToggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
-    <div>
-      <h1>Theme Switch</h1>
+    <div className="theme-switch-container" data-theme={theme}>
+      <h1 className="header">Theme Switch</h1>
+      <div className="container">
+        <p>Hello World!</p>
+        <button onClick={handleToggleTheme} className="change-theme-button">
+          Change Theme
+        </button>
+      </div>
     </div>
   );
 };
---
+++ theme-switch/src/components/useLocalStorage.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+// custom hook to store and get the theme from localStorage
+const useLocalStorage = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    let currentValue;
+
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || String(defaultValue),
+      );
+    } catch (error) {
+      console.log(error);
+      currentValue = defaultValue;
+    }
+
+    return currentValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
