dfb70da15fe85e81093c6099675078b24385b341
Anmol Shah <anmolsha80@gmail.com>
2024-09-24 23:04:23 +0545

    Handle error statuses using switch case instead of if...else statements

--- food-recipe-app/src/lib/SearchRecipe.js
+++ food-recipe-app/src/lib/SearchRecipe.js
@@ -76,11 +76,16 @@ const searchRecipeByID = async (
     } else {
       const { status } = response;
 
-      if (status === 404) throw new Error('404, Not Found');
-
-      if (status === 500) throw new Error('500, Internal Server Error');
-
-      throw new Error(status);
+      switch (status) {
+        case 401:
+          throw new Error('401, Unauthorized');
+        case 404:
+          throw new Error('404, Not Found');
+        case 500:
+          throw new Error('500, Internal Server Error');
+        default:
+          throw new Error(status);
+      }
     }
   } catch (error) {
     setErrorMessage(error.message);
